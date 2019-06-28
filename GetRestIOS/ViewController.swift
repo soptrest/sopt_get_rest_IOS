3b68fb4c646aaa0cac334d3976d4d17a4426804e
leean <leeeeean91@gmail.com>
2019-06-28 18:27:03 +0900

    Initial Commit

---
+++ GetRestIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetRestIOS
+//
+//  Created by 최리안 on 28/06/2019.
+//  Copyright © 2019 최리안. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
