6a8768e1a67d9a5e923109827119d029f85a78c7
박경선 <rudtjs4540@gmail.com>
2019-07-01 03:22:49 +0900

    Navigation Bar and Top Side View design

--- GetRestIOS/Controllers/MainHomeViewController.swift
+++ GetRestIOS/Controllers/MainHomeViewController.swift
@@ -9,28 +9,40 @@
 import UIKit
 
 class MainHomeViewController: UIViewController {
-
+    
     @IBOutlet weak var topSideView: UIView!
+    @IBOutlet weak var TopSideLabel: UILabel!
+    
+    var username: String = "박경선"
+    
+    //    let fontSize = UIFont.boldSystemFont(ofSize: 30)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let scrollView = UIScrollView(frame: view.bounds)
+        view.addSubview(scrollView)
+        
         navigationController?.navigationBar.barTintColor = UIColor.mainGreen
         navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage()
         
         topSideView.backgroundColor = UIColor.mainGreen
         topSideView.roundCorners(corners: [.bottomLeft, .bottomRight], radius: 28)
-
+        
+        TopSideLabel.text = "\(username)님의\n기록을 살펴볼까요?"
+        TopSideLabel.numberOfLines = 0
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func attributedText(withString string: String, boldString: String, font: UIFont) -> String {
+        let attributedString = NSMutableAttributedString(string: string,
+                                                         attributes: [NSAttributedString.Key.font: font])
+        let boldFontAttribute: [NSAttributedString.Key: Any] = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: font.pointSize)]
+        let range = (string as NSString).range(of: boldString)
+        attributedString.addAttributes(boldFontAttribute, range: range)
+        return attributedString.string
     }
-    */
-
+    
 }
