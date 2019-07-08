c1f4c3b477493562c9088e2f11bc7a1d5c0b9f96
leean <leeeeean91@gmail.com>
2019-07-08 10:56:25 +0900

    tableViewCell

--- GetRestIOS/Controllers/PortfolioDetailVC1.swift
+++ GetRestIOS/Controllers/PortfolioDetailVC1.swift
@@ -10,14 +10,11 @@ import UIKit
 import XLPagerTabStrip
 
 class PortfolioDetailVC1: UIViewController, IndicatorInfoProvider {
-    var pfimg: [UIImage?] = [UIImage(named: "icImg"),UIImage(named: "icImg"),UIImage(named: "icImg"),UIImage(named: "icImg"),UIImage(named: "icImg"),UIImage(named: "icImg"),UIImage(named: "icImg")]
     
-    var pftitle: [String] = ["sopq1", "sopq2", "sopq5", "sopq6", "sopq9", "sopq10", "sopq11"]
-    var pfdate: [String] = ["2019.03 ~ 2019.07", "2019.03 ~ 2019.07", "2019.03 ~ 2019.07", "2019.03 ~ 2019.07","2019.03 ~ 2019.07","2019.03 ~ 2019.07","2019.03 ~ 2019.07"]
-   
     @IBOutlet var portfolioTableView: UITableView!
    
     var tabTitle: String = ""
+    var list: [PortfoliolModel] = []
 
     override func viewDidLoad() {
         
@@ -28,6 +25,7 @@ class PortfolioDetailVC1: UIViewController, IndicatorInfoProvider {
         
         let nibName = UINib(nibName: "PortfolioTableViewCell", bundle: nil)
         portfolioTableView.register(nibName, forCellReuseIdentifier: "PortfolioCell")
+        setData()
     }
     
     override func didReceiveMemoryWarning() {
@@ -36,26 +34,39 @@ class PortfolioDetailVC1: UIViewController, IndicatorInfoProvider {
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
         return IndicatorInfo(title: "\(tabTitle)")
     }
-
-
-
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        if let index = portfolioTableView.indexPathForSelectedRow {
+            portfolioTableView.deselectRow(at: index, animated: true)
+        }
+    }
 }
 
 extension PortfolioDetailVC1: UITableViewDelegate, UITableViewDataSource {
     
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pftitle.count
+        return list.count
     }
+    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 160
+//    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = portfolioTableView.dequeueReusableCell(withIdentifier: "PortfolioCell", for: indexPath) as! PortfolioTableViewCell
         
-        cell.portfolioTitle.text = pftitle[indexPath.row]
-        cell.portfolioDate.text = pfdate[indexPath.row]
-        cell.portfolioImg.image = UIImage(named: "icImg")
-        cell.portfolioTitle.sizeToFit()
+        let data = list[indexPath.row]
         
+        if indexPath.row == 1 {
+            cell.backgroundColor = .black
+            
+        }
+        cell.portfolioTitle.text = data.pfTitle
+        cell.portfolioDate.text = data.pfDate
+        cell.portfolioImg.image = data.pfImg
+//        cell.portfolioTitle.sizeToFit()
         
         return cell
     }
@@ -74,7 +85,6 @@ extension PortfolioDetailVC1: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        
         return true
     }
     
@@ -83,17 +93,32 @@ extension PortfolioDetailVC1: UITableViewDelegate, UITableViewDataSource {
          테이블 뷰 의 row 를 변화시키면 그에 따라 대응되는 모델(데이터)도 변화시켜주어야 합니다.
          sourceIndexPath와 destinationIndexPath를 통해 이동을 시작한 index와 새롭게 설정된 index를 가져올 수 있습니다.
          */
-//        let movingIndex = musicList[sourceIndexPath.row]
-//
-//        musicList.remove(at: sourceIndexPath.row)
-//        musicList.insert(movingIndex, at: destinationIndexPath.row)
+        let movingIndex = list[sourceIndexPath.row]
+
+        list.remove(at: sourceIndexPath.row)
+        list.insert(movingIndex, at: destinationIndexPath.row)
     }
     
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            musicList.remove(at: indexPath.row)
-//            tableView.deleteRows(at: [indexPath], with: .automatic)
-//        }
-//    }
-    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            list.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
+
+extension PortfolioDetailVC1 {
+    func setData() {
+        let pf1 = PortfoliolModel(img: "icImg", title: "솝트1", date: "2019.03 ~ 2019.07")
+        let pf2 = PortfoliolModel(img: "icImg", title: "솝트2", date: "2019.03 ~ 2019.07")
+        let pf3 = PortfoliolModel(img: "icImg", title: "솝트3", date: "2019.03 ~ 2019.07")
+        let pf4 = PortfoliolModel(img: "icImg", title: "솝트4", date: "2019.03 ~ 2019.07")
+        let pf5 = PortfoliolModel(img: "icImg", title: "솝트5", date: "2019.03 ~ 2019.07")
+        let pf6 = PortfoliolModel(img: "icImg", title: "솝트6", date: "2019.03 ~ 2019.07")
+        let pf7 = PortfoliolModel(img: "icImg", title: "솝트7", date: "2019.03 ~ 2019.07")
+        let pf8 = PortfoliolModel(img: "icImg", title: "솝트8", date: "2019.03 ~ 2019.07")
+        
+        list = [pf1, pf2, pf3, pf4, pf5, pf6, pf7, pf8]
+    }
+}
+
--- GetRestIOS/Views/PortfolioTableViewCell.swift
+++ GetRestIOS/Views/PortfolioTableViewCell.swift
@@ -18,6 +18,7 @@ class PortfolioTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        portfolioImg.layer.cornerRadius = 5
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
