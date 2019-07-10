62a4cf4abff174c51d7a41d1ddf17a74cb03e77e
leean <leeeeean91@gmail.com>
2019-07-10 20:18:27 +0900

    merge-modify
    머지를 위해 띄어쓰기 하나~~~

--- GetRestIOS/Controllers/MainHomeViewController.swift
+++ GetRestIOS/Controllers/MainHomeViewController.swift
@@ -86,7 +86,7 @@ class MainHomeViewController: UIViewController {
     
     func attributedText(withString string: String, boldString: String, font: UIFont) -> String {
         let attributedString = NSMutableAttributedString(string: string, attributes: [NSAttributedString.Key.font: font])
-        let boldFontAttribute: [NSAttributedString.Key: Any] = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: font.pointSize)]
+        let boldFontAttribute: [NSAttributedString.Key: Any] = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: font.pointSize)] 
         let range = (string as NSString).range(of: boldString)
         attributedString.addAttributes(boldFontAttribute, range: range)
         return attributedString.string
